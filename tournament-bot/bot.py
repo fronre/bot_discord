7b800b3c4c99272fc0058af43f4282f0d651623c
hala mohammed  islam <149974305+fronre@users.noreply.github.com>
2025-11-03 22:38:33 +0100

    Update bot.py

--- tournament-bot/bot.py
+++ tournament-bot/bot.py
@@ -1,22 +1,19 @@
 import discord
 from discord.ext import commands
 
-# استبدل التوكن هنا 🔒
-TOKEN = "YOUR_BOT_TOKEN"
 
-# تعريف البوت
+
 intents = discord.Intents.default()
 intents.message_content = True  # ضروري باش يقرا الرسائل
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# قائمة اللاعبين
 players = []
 
 @bot.event
 async def on_ready():
     print(f"✅ Logged in as {bot.user}")
 
-# أمر التسجيل
+# 
 @bot.command()
 async def join(ctx):
     username = ctx.author.name
